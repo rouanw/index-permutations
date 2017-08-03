122aa3ce79e5710c12dee59264951cd20a791178
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2017-08-03 13:41:54 +0200

    find all combinations for all permutations of terms used in a query

--- index-permutations.js
+++ index-permutations.js
@@ -1,4 +1,8 @@
+const Combinatorics = require('js-combinatorics');
+
 module.exports = (query) => {
   const keys = Object.keys(query);
-  return keys.map((key) => ({ [key]: 1 }));
+  const indexKeys = keys.map((key) => ({ [key]: 1 }));
+  const combinations = Combinatorics.permutationCombination(indexKeys);
+  return combinations.map((combination) => Object.assign({}, ...combination));
 };
--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const test = require('tape');
+const _ = require('lodash');
 const indexPermutations = require('./index-permutations');
 
 test('index permutations', function (t) {
-  t.plan(2);
+  t.plan(3);
 
   t.deepEqual(indexPermutations({ 'name.first': 'Richard' }), [
     { 'name.first': 1 },
@@ -11,7 +12,29 @@ test('index permutations', function (t) {
   t.deepEqual(indexPermutations({ 'name.first': 'Richard', 'vegan': true }), [
     { 'name.first': 1 },
     { vegan: 1 },
+    { 'name.first': 1, vegan: 1 },
+    { vegan: 1, 'name.first': 1 },
   ]);
+
+  const expected = [
+    { 'name.first': 1 },
+    { vegan: 1 },
+    { happy: 1 },
+    { 'name.first': 1, happy: 1 },
+    { vegan: 1, 'name.first': 1 },
+    { happy: 1, 'name.first': 1 },
+    { happy: 1, vegan: 1 },
+    { happy: 1, vegan: 1 },
+    { vegan: 1, happy: 1 },
+    { happy: 1, vegan: 1, 'name.first': 1},
+    { happy: 1, 'name.first': 1, vegan: 1},
+    { 'name.first': 1, vegan: 1, happy: 1 },
+    { 'name.first': 1, happy: 1, vegan: 1 },
+    { vegan: 1, 'name.first': 1, happy: 1},
+    { vegan: 1, happy: 1, 'name.first': 1},
+  ];
+  const actual = indexPermutations({ 'name.first': 'Richard', 'vegan': true, happy: false });
+  t.ok(_(actual).differenceWith(expected, _.isEqual).isEmpty());
 
   t.end();
 });
